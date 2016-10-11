2cbda41608ab377014c2a263f1ddb1f039c9a2a3
LiuTianqing <lhard@qq.com>
2016-10-11 16:42:07 +0800

    init study init

---
+++ Model/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Customer
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsFirstTimeLogin { get; set; }
+        public int AccessFailedCount { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsActive { get; set; }
+        public Role Role { get; set; }
+    }
+}
